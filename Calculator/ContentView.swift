d40cdd562b4d9b83034962e950305440d7eab053
Hammad Ali <hammadali78056@gmail.com>
2025-09-25 16:31:41 +0500

    feat :  build calculator app

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,14 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
+   // @State private var name:String = "Calculator"
+    @State private var displayText: String = "0"
+    @State private var currentInput: String = ""
+    @State private var storedValue: Double? = 0
+    @State private var currentOperator: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.black).ignoresSafeArea()
+            
+            
+            VStack {
+                //Text(name)
+                    //.font(.system(size: 40))
+                    //.foregroundColor(Color.white)
+                Spacer()
+                Text(displayText)
+                                .font(.system(size: 80))
+                                .foregroundColor(Color.white)
+                                .padding(.trailing, 20)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                calculatorbtnRow(buttons: ["7","8","9","/"], action: buttonTap)
+                calculatorbtnRow(buttons: ["4","5","6","*"], action: buttonTap)
+                calculatorbtnRow(buttons: ["1","2","3","-"], action: buttonTap)
+                calculatorbtnRow(buttons: [".","0","=","+"], action: buttonTap)
+                calculatorbtnRow(buttons: ["C"], action: {button in clearbtn()})
+                
+
+            }
+            .padding()
         }
-        .padding()
+        
+    }
+    func buttonTap(_ button:String){
+        switch button{
+        case "+","-","*","/" :
+            currentOperator = button
+            storedValue = Double(currentInput)
+            currentInput = ""
+        case "=" :
+            if let operatorType = currentOperator,
+         let storedValue = storedValue,
+                let CurrentValue = Double(currentInput){
+                    let result:Double
+                    
+                    switch operatorType{
+                    case "+" :
+                        result = storedValue + CurrentValue
+                    case "-" :
+                        result = storedValue - CurrentValue
+                    case "*" :
+                        result = storedValue * CurrentValue
+                    case "/" :
+                        result = storedValue / CurrentValue
+                    default :
+                        fatalError("Unknown Operator")
+                    }
+                    currentInput = "\(result)"
+                    currentOperator = nil
+                }
+        case "." :
+            currentInput += button
+        default:
+            currentInput += button
+        }
+        if currentInput.isEmpty{
+            displayText = "0"
+        } else {
+            displayText = currentInput
+        }
+        
+    }
+    func clearbtn(){
+        currentInput = ""
+        storedValue = nil
+        currentOperator = nil
+        displayText = "0"
+    }
+    
+}
+
+
+
+
+struct calculatorbtnRow: View{
+    let buttons: [String]
+    let action: (String) ->Void
+    let buttonSize = (UIScreen.main.bounds.width - 5 * 12) / 4
+   
+    
+    
+    var body: some View{
+        
+        HStack(spacing: 12){
+            ForEach(buttons, id: \.self){ buttonLabel in
+                Button(action: {
+                    action(buttonLabel)
+                }) {
+                    Text(buttonLabel)
+                        .font(.title)
+                        .frame(width: buttonSize, height: buttonSize)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(buttonSize)
+                }
+                
+                
+            }
+            
+            
+        }
     }
 }
 
