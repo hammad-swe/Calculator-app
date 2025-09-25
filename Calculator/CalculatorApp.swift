c2b80b5216b001f92684357395ca73f286feb311
Hammad Ali <hammadali78056@gmail.com>
2025-09-25 08:03:47 +0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Hammad Ali on 25/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
